1ea34fb0646d48a099a1c6b5cc53e2fa7afb396b
wanghang <100349018wh@gmail.com>
2018-07-09 10:02:37 +0800

    添加了ArticleDTO.java

---
+++ src/main/java/com/uestc/www/pojo/dto/article/ArticleDTO.java
@@ -0,0 +1,66 @@
+package com.uestc.www.pojo.dto.article;
+
+import java.util.Date;
+
+public class ArticleDTO {
+
+    private Integer id;
+
+    private String articleId;
+
+    private String articleType;
+
+    private String articleTitle;
+
+    private Date createTime;
+
+    private Date modifyTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(String articleId) {
+        this.articleId = articleId == null ? null : articleId.trim();
+    }
+
+    public String getArticleType() {
+        return articleType;
+    }
+
+    public void setArticleType(String articleType) {
+        this.articleType = articleType == null ? null : articleType.trim();
+    }
+
+    public String getArticleTitle() {
+        return articleTitle;
+    }
+
+    public void setArticleTitle(String articleTitle) {
+        this.articleTitle = articleTitle == null ? null : articleTitle.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+}
